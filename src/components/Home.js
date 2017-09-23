b5227db50ef41eed1320f669fcda7f27c50e4396
Luis Carlos Martinez <lcmartinezl05@gmail.com>
2017-09-22 22:52:49 -0500

    Pass props to components

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,22 @@ export class Home extends React.Component {
     return (
       <div className="container-fluid">
         <div className="row">
-        <div className="col-md-12">
-          Lorem ipsum dolor sit amet consectetur adipisicing
-        </div>
+          <div className="col-md-12">
+            <p>
+              Your name is: {this.props.name}, your age is: {this.props.age}
+            </p>
+            <div>
+              <h4>Hoobies:</h4>
+              <ul>
+                {
+                  this.props.user.hobbies.map(
+                    (hobby, i) => <li key={i}>{hobby}</li>
+                  )
+                }
+              </ul>
+            </div>
+            {this.props.children}
+          </div>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -6,16 +6,27 @@ import { Home } from './components/Home';
 
 class App extends React.Component {
   render() {
+    let user = {
+      name: "Luis Martinez",
+      hobbies: [
+        'soccer',
+        'web development'
+      ]
+    }
     return (
       <div>
         <Header/>
-        <Home/>
+        <Home
+          name={"LCM"}
+          age={27}
+          user={user}
+        />
       </div>
     );
   };
 };
 
 render(
-  <App/>, 
+  <App/>,
   document.getElementById('app')
 );
