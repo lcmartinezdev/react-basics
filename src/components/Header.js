23574b4138dc7c460f29effa4c692ea156ea6d8a
Luis Carlos Martinez <lcmartinezl05@gmail.com>
2017-09-22 22:11:18 -0500

    Indetantion fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,20 @@ import React from 'react'
 
 export class Header extends React.Component {
   render() {
-      return (
-        <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <div>
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-      );
+    return (
+      <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 export class Home extends React.Component {
   render() {
-      return (
-          <div className="container-fluid">
-              <div className="row">
-                <div className="col-md-12">
-                    Lorem ipsum dolor sit amet consectetur adipisicing
-                </div>
-              </div>
-          </div>
-      );
+    return (
+      <div className="container-fluid">
+        <div className="row">
+        <div className="col-md-12">
+          Lorem ipsum dolor sit amet consectetur adipisicing
+        </div>
+        </div>
+      </div>
+    );
   }
 }
