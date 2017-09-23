015daebb16547ca3966efed1bf61458077c32093
Luis Carlos Martinez <lcmartinezl05@gmail.com>
2017-09-22 22:02:42 -0500

    Add styles to components

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,12 @@ import React from 'react'
 export class Home extends React.Component {
   render() {
       return (
-          <div>
-              Home
+          <div className="container-fluid">
+              <div className="row">
+                <div className="col-md-12">
+                    Lorem ipsum dolor sit amet consectetur adipisicing
+                </div>
+              </div>
           </div>
       );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {Header} from './components/Header';
-import {Home} from './components/Home';
+import { render } from 'react-dom';
+import { Header } from './components/Header';
+import { Home } from './components/Home';
 
 
 class App extends React.Component {
